1b40b1e513ada821ef36781f91e9c29e41b12f40
Stefan Koch <stefan.koch@micromeritics.com>
2020-04-08 13:11:44 -0400

    Add (python) full coverage testing.

---
+++ react_rest_django/management/commands/coverage.py
@@ -0,0 +1,24 @@
+"""Command handler for test coverage on the whole project."""
+import json
+import subprocess
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    """Command handler for coverage on the whole project."""
+    help = 'Runs coverage on all the (python) source code of the project.'
+
+    def handle(self, *args, **options):
+        # Make sure all the tests pass.
+        subprocess.call(['coverage', 'erase'])
+        ret = subprocess.call(['coverage', 'run', '--source=.', 'manage.py', 'test'])
+        if ret:
+            raise CommandError("Code not Tests did not pass.")
+
+        # if there were no errors, show the results and look for coverage.
+        subprocess.call(['coverage', 'html'])
+        subprocess.call(['coverage', 'report'])
+        data = json.loads(
+            subprocess.run(['coverage', 'json', '-o', '-'],
+                           capture_output=True, check=True).stdout)
+        if data['totals']['missing_lines'] > 0:
+            raise CommandError("Code not 100% coverage")
---
+++ react_rest_django/tests/test_commands.py
@@ -0,0 +1,50 @@
+"""Tests for the system level commands"""
+from collections import namedtuple
+from unittest import mock
+
+from django.test import TestCase
+from django.core.management import call_command
+from django.core.management.base import CommandError
+
+# Create your tests here.
+class LintTest(TestCase):
+    """Tests for the lint command"""
+
+    @mock.patch('subprocess.call')
+    def test_command_calls(self, mock_call):
+        """Make sure errors in lint cause Command Exceptions."""
+
+        # raise exception on pylint errors.
+        mock_call.side_effect = lambda args: 'pylint' in args[0]
+        self.assertRaises(CommandError, call_command, 'lint')
+
+        # raise exception on tslint errors.
+        mock_call.side_effect = lambda args: 'tslint' in args[0]
+        self.assertRaises(CommandError, call_command, 'lint')
+
+        # no errors, no exceptions.
+        mock_call.side_effect = lambda args: 0
+        call_command('lint')
+
+class CoverageTest(TestCase):
+    """Tests for the coverage command"""
+
+    @mock.patch('subprocess.run')
+    @mock.patch('subprocess.call')
+    def test_command_calls(self, mock_call, mock_run):
+        """Make sure errors in lint cause Command Exceptions."""
+
+        # make sure failing tests do not show report.
+        mock_call.side_effect = lambda args: 'run' in args[1]
+        self.assertRaises(CommandError, call_command, 'coverage')
+        mock_run.assert_not_called()
+
+        # make sure assert if there is not 100% coverage.
+        mock_call.side_effect = lambda args: 0
+        mock_run.return_value = namedtuple("mock", ["stdout"])(stdout='{"totals": {"missing_lines": 2}}')
+        self.assertRaises(CommandError, call_command, 'coverage')
+
+        # make sure no assert if there is 100% coverage.
+        mock_call.side_effect = lambda args: 0
+        mock_run.return_value = namedtuple("mock", ["stdout"])(stdout='{"totals": {"missing_lines": 0}}')
+        call_command('coverage')
