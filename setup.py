686de6dd28ad029e9493cd47f603b6607b8ca0d8
Stefan Koch <stefan.koch@micromeritics.com>
2020-04-16 13:07:08 -0400

    Fixes to allow it to upload to pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.8',
     install_requires=[
         'pylint', "pylint_django",
         "coverage",
